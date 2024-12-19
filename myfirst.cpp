f3cfe4c2c67da0203af0b768b801138df30d16ea
verkhusha <verkhusha.vladyslav@lll.kpi.ua>
2024-12-19 10:51:15 +0200

    test

--- myfirst.cpp
+++ myfirst.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
-int main ()
-{
-     using namespace std;
-     cout << "Come up and C++ me some time.";
-     cout << endl;
-     cout << "You wont regret it" << endl;
-     return 0;
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Введіть сть елементів у масиві: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Введіть елементи масиву: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    int maxIndex = 0, minIndex = 0;
+
+    // Знаходимо індекси максимального та мінімального елементів
+    for (int i = 1; i < n; ++i) {
+        if (arr[i] > arr[maxIndex]) {
+            maxIndex = i;
+        }
+        if (arr[i] < arr[minIndex]) {
+            minIndex = i;
+        }
+    }
+
+    // Міняємо місцями максимальний і мінімальний елементи
+    swap(arr[maxIndex], arr[minIndex]);
+
+    cout << "Масив після обміну: ";
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
